fac7a487c4334033624178bf1a65661535909cf4
gleb165 <83755952+gleb165@users.noreply.github.com>
2025-04-15 21:48:33 +0300

    functionality for sharing

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.urls import reverse
+# Create your models here.
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=200)
+    slog = models.CharField(max_length=200, unique_for_date='publish')
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_urls(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slog])
